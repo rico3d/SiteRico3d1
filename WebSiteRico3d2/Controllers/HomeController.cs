63ad5ac7db4993b84d1ce5a38eb01e21c79560df
rico3d <rico3d2001@gmail.com>
2015-12-26 17:11:15 -0200

    BD Funcina WEB

--- WebSiteRico3d2/Controllers/HomeController.cs
+++ WebSiteRico3d2/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrightstarDB.Client;
 using Mosaicos.LojaVirtual.Dominio.Repositorio;
+using VDS.RDF.Storage;
 using WebSiteRico3d2.Models;
 
 namespace WebSiteRico3d2.Controllers
@@ -20,15 +22,18 @@ namespace WebSiteRico3d2.Controllers
         public ViewResult About(int pagina = 1)
         {
             ViewBag.Message = "Your application description page.";
+            string path = HttpContext.Server.MapPath("~/App_Data/");
 
-            _loja = new LojaMosaicosContext();
-            _loja.Mosaicos.Add(new Mosaico()
-            {
-               Descricao = "NewDescricao",
-               Nome = "NewNome",
-             });
+            var connectionString = "type=embedded;storesdirectory=" + path + "brightstar;storename=test4";
+            _loja = new LojaMosaicosContext(connectionString);
+             
+            //_loja.Mosaicos.Add(new Mosaico()
+            //{
+               //Descricao = "NewDescricao",
+               //Nome = "NewNome",
+             //});
 
-            _loja.SaveChanges();
+            //_loja.SaveChanges();
 
             var model = new MosaicosViewModel()
             {
