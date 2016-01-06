bbc8771384458e1b22de20cad53498d5e4b0aca5
rico3d <rico3d2001@gmail.com>
2016-01-05 22:36:01 -0200

    testes andanebto

---
+++ WebSiteRico3d2/DominioMosaico/ItemCarrinho.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mosaicos.LojaVirtual.Dominio.Repositorio;
+
+namespace WebSiteRico3d2.DominioMosaico
+{
+    public class ItemCarrinho
+    {
+
+        public IMosaico Produto { get; set; }
+
+        public int Quantidade { get; set; }
+
+    }
+}
