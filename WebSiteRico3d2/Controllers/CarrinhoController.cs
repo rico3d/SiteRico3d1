7d49fab29d192bae170a379fc66eb651be042c0f
rico3d <rico3d2001@gmail.com>
2016-01-06 19:56:16 -0200

    Aula18

---
+++ WebSiteRico3d2/Controllers/CarrinhoController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Mosaicos.LojaVirtual.Dominio.Repositorio;
+using WebSiteRico3d2.DominioMosaico;
+
+namespace WebSiteRico3d2.Controllers
+{
+
+
+    public class CarrinhoController : Controller
+    {
+        // GET: Carrinho
+        public RedirectToRouteResult Adicionar(string produtoId, string returnUrl)
+        {
+            var caminho = HttpContext.Server.MapPath("~/App_Data/");
+            var connectionString1 = "type=embedded;storesdirectory=" + caminho + "brightstar;storename=test5";
+            var repositorio = new LojaMosaicosContext(connectionString1);
+
+            var mosaico = (Mosaico) repositorio.Mosaicos.FirstOrDefault(p => p.Id == produtoId);
+            if (mosaico != null)
+            {
+                ObterCarrinho().AdicionarItem(mosaico, 1);
+            }
+
+            return RedirectToAction("ListaMosaicos", new {returnUrl});
+
+        }
+
+        private Carrinho ObterCarrinho()
+        {
+            var carrinho = (Carrinho) Session["Carrinho"];
+
+            if (carrinho == null)
+            {
+                carrinho = new Carrinho();
+                Session["Carrinho"] = carrinho;
+            }
+
+            return carrinho;
+        }
+
+        public RedirectToRouteResult Remover(string produtoId, string returnUrl)
+        {
+            
+            var caminho = HttpContext.Server.MapPath("~/App_Data/");
+            var connectionString1 = "type=embedded;storesdirectory=" + caminho + "brightstar;storename=test5";
+            var repositorio = new LojaMosaicosContext(connectionString1);
+
+            var mosaico = (Mosaico)repositorio.Mosaicos.FirstOrDefault(p => p.Id == produtoId);
+
+            if (mosaico != null)
+            {
+                ObterCarrinho().RemoverItem(mosaico);
+            }
+
+            return RedirectToAction("ListaMosaicos", new {returnUrl});
+
+        }
+
+
+    }
+}
