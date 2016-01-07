a4a30fb95dc9d9538dc7985f6039146059853483
rico3d <rico3d2001@gmail.com>
2016-01-07 20:55:42 -0200

    Aula 20

--- WebSiteRico3d2/Controllers/CarrinhoController.cs
+++ WebSiteRico3d2/Controllers/CarrinhoController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Web.Routing;
 using Mosaicos.LojaVirtual.Dominio.Repositorio;
 using WebSiteRico3d2.DominioMosaico;
+using WebSiteRico3d2.Models;
 
 namespace WebSiteRico3d2.Controllers
 {
@@ -14,19 +15,19 @@ namespace WebSiteRico3d2.Controllers
     public class CarrinhoController : Controller
     {
         // GET: Carrinho
-        public RedirectToRouteResult Adicionar(string produtoId, string returnUrl)
+        public RedirectToRouteResult Adicionar(string Id, string returnUrl)
         {
             var caminho = HttpContext.Server.MapPath("~/App_Data/");
             var connectionString1 = "type=embedded;storesdirectory=" + caminho + "brightstar;storename=test5";
             var repositorio = new LojaMosaicosContext(connectionString1);
 
-            var mosaico = (Mosaico) repositorio.Mosaicos.FirstOrDefault(p => p.Id == produtoId);
+            var mosaico = (Mosaico) repositorio.Mosaicos.FirstOrDefault(p => p.Id == Id);
             if (mosaico != null)
             {
                 ObterCarrinho().AdicionarItem(mosaico, 1);
             }
 
-            return RedirectToAction("ListaMosaicos", new {returnUrl});
+            return RedirectToAction("Indice", new {returnUrl});
 
         }
 
@@ -43,22 +44,37 @@ namespace WebSiteRico3d2.Controllers
             return carrinho;
         }
 
-        public RedirectToRouteResult Remover(string produtoId, string returnUrl)
+        public RedirectToRouteResult Remover(string Id, string returnUrl)
         {
             
             var caminho = HttpContext.Server.MapPath("~/App_Data/");
             var connectionString1 = "type=embedded;storesdirectory=" + caminho + "brightstar;storename=test5";
             var repositorio = new LojaMosaicosContext(connectionString1);
 
-            var mosaico = (Mosaico)repositorio.Mosaicos.FirstOrDefault(p => p.Id == produtoId);
+            var mosaico = (Mosaico)repositorio.Mosaicos.FirstOrDefault(p => p.Id == Id);
 
             if (mosaico != null)
             {
                 ObterCarrinho().RemoverItem(mosaico);
             }
 
-            return RedirectToAction("ListaMosaicos", new {returnUrl});
+            return RedirectToAction("Indice", new {returnUrl});
 
+        }
+
+        public ViewResult Indice(string returnUrl)
+        {
+            return View(new CarrinhoViewModel()
+            {
+                Carrinho = ObterCarrinho(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public PartialViewResult Resumo()
+        {
+            var carrinho = ObterCarrinho();
+            return PartialView(carrinho);
         }
 
 
---
+++ WebSiteRico3d2/Models/CarrinhoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebSiteRico3d2.DominioMosaico;
+
+namespace WebSiteRico3d2.Models
+{
+    public class CarrinhoViewModel
+    {
+
+        public Carrinho Carrinho { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
