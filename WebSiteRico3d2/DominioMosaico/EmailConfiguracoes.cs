a01344aa2c6fe97e6d0a8c952e99e9cafbafd817
rico3d <rico3d2001@gmail.com>
2016-02-05 06:45:36 -0200

    Quase email

---
+++ WebSiteRico3d2/DominioMosaico/EmailConfiguracoes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSiteRico3d2.DominioMosaico
+{
+    public class EmailConfiguracoes
+    {
+        public bool UsarSsl = false;
+
+        public string ServidorSmtp = "mail39.redehost.com.br";
+
+        public int ServidorPorta = 587;
+
+        public string Usuario = "ricardo.pinto@rico3d.com.br";
+
+        public bool EscreverArquivo = false;
+
+        public string PastaArquivo = @"c:\envioemail";
+
+        public string De = "ricardo.pinto@rico3d.com.br";
+
+        public string Para = "ricardo.pinto@rico3d.com.br";
+
+        public string Senha = "PqpTnc@45";
+
+    }
+}
